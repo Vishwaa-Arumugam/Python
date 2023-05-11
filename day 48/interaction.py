ca17925af66c77e70e1b6dd5edea36005a4ddf0d
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2023-05-11 11:41:39 +0530

    Add files via upload

---
+++ day 48/interaction.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+chrome_driver_path = "C:\\Users\\Vishwaa Arumugam\\Desktop\\chromedriver.exe"
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.stealmylogin.com/demo.html")
+
+search = driver.find_element(By.NAME, "username")
+search.send_keys("Python")
+
+pas = driver.find_element(By.NAME, "password")
+pas.send_keys("hhh")
+
+b = driver.find_element(By.XPATH, "/html/body/form/input[3]")
+b.click()
+
+sleep(3)
+driver.quit()
