28b3250846082b0fb1e5bbb9b6c3ee36e4279f4e
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2023-05-26 19:48:39 +0530

    Update main.py

--- day 30/main.py
+++ day 30/main.py
@@ -1,18 +1,19 @@
+# 
 
-# try:
-#     file = open("file.txt")
-#     dictionary = {"key" : "value"}
-#     print(dictionary["key"])
-# except FileNotFoundError:
-#     file = open("file.txt","w")
-#     file.write("Something")
-# except KeyError as error_mesage:
-#     print(f"The key {error_mesage} doesn't exist")
+try:
+    file = open("file.txt")
+    dictionary = {"key" : "value"}
+    print(dictionary["key"])
+except FileNotFoundError:
+    file = open("file.txt","w")
+    file.write("Something")
+except KeyError as error_mesage:
+    print(f"The key {error_mesage} doesn't exist")
 
-# else:
-#     content = file.read()
-#     print(content)
+else:
+    content = file.read()
+    print(content)
 
-# finally:
-#     print("Finally will always execute")
+finally:
+    print("Finally will always execute")
 
