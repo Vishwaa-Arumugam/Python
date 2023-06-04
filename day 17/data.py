a715fd21aa4b8986d3519261c9f7b20c318c8735
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2023-06-04 19:47:08 +0530

    Update data.py

--- day 17/data.py
+++ day 17/data.py
@@ -10,5 +10,4 @@ question_data = [
 {"text": "Google was originally called 'Backrub'.", "answer": "True"},
 {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
 {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
